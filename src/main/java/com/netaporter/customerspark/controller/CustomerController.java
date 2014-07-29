32c823df4a6b599cf2291423d41ab1df755e030b
jameshoare <jhoare0@gmail.com>
2014-07-29 20:55:18 +0100

    polish

--- src/main/java/com/netaporter/customerspark/controller/CustomerController.java
+++ src/main/java/com/netaporter/customerspark/controller/CustomerController.java
@@ -4,7 +4,7 @@ package com.netaporter.customerspark.controller;
 import com.netaporter.customerspark.services.CustomerService;
 import com.netaporter.customerspark.transformers.JsonTransformer;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 /**
  * Created by jameshoare on 21/07/2014.
@@ -13,10 +13,25 @@ public class CustomerController {
 
     public CustomerController(final CustomerService customerService) {
 
-        get("/customer/:id", "application/json", (req, res) -> {
+
+        get("/customer/:id", (req, res) -> {
             String id = req.params(":id");
             return customerService.getCustomer(id);
         }, new JsonTransformer());
+
+        /**
+         * e.g  curl -XPOST http://localhost:4567/customer?name=James&email=james.hoare@net-a-porter.com
+         */
+        post("/customer", (req, res) -> customerService.createCustomer(
+                req.queryParams("name"),
+                req.queryParams("email")
+        ), new JsonTransformer());
+
+        put("/customer/:id", (req, res) -> customerService.updateCustomer(
+                req.params(":id"),
+                req.queryParams("name"),
+                req.queryParams("email")
+        ), new JsonTransformer());
 
 
     }
